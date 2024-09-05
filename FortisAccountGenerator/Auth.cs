ef9e9eea5e3311f7e171d7d8678b3b9f2d52eb2e
Kirill Yartsev <kirill.yartsev@tiger.trade>
2024-09-06 01:16:23 +0300

    Added Token generate POST Method

---
+++ FortisAccountGenerator/Auth.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace FortisAccountGenerator;
+
+public class Auth
+{
+    public static string TokenGet(string username, string password)
+    {
+        Dictionary<string, string> body = new Dictionary<string, string>();
+
+        body.Add("grant_type", "password");
+        body.Add("client_id", "fortis-onboard");
+        body.Add("username", username);
+        body.Add("password", password);
+
+       // var content = new FormUrlEncodedContent(body);
+        RestResponse result = SingIn(body: body).Result;
+        JObject jobject = (JObject) null;
+        try
+        {
+            jobject = JObject.Parse(result.Content);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Message: {ex.Message} \r\n StackTrace:{ex.StackTrace}");
+            throw;
+        }
+        
+        var accessToken = jobject["access_token"].ToString();
+        return accessToken;
+    }
+
+    private static async Task<RestResponse> SingIn(
+        Dictionary<string, string> pathParams = null,
+        Dictionary<string, string> headerParams = null,
+        Dictionary<string, string> queryParams = null,
+        Dictionary<string, string> body = null)
+    {
+        string devApiHostName = "https://keycloak-dev.http.dev.fortisds.tech";
+        string endpoint = "/realms/fortis/protocol/openid-connect/token";
+
+        RestClient client = new RestClient(devApiHostName);
+        RestRequest request = new RestRequest(endpoint);
+        request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.AddParameter("application/x-www-form-urlencoded",
+            $"grant_type={body["grant_type"]}&client_id={body["client_id"]}&username={body["username"]}&password={body["password"]}", ParameterType.RequestBody);
+
+        RestResponse response = await client.ExecutePostAsync(request);
+
+        devApiHostName = (string) null;
+        endpoint = (string) null;
+        client = (RestClient) null;
+        request = (RestRequest) null;
+
+        return response;
+    }
+}
--- FortisAccountGenerator/Program.cs
+++ FortisAccountGenerator/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace FortisAccountGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContextPrepare();
+
+        }
+
+        private static Dictionary<string, object> ContextPrepare()
+        {
+            /*Console.WriteLine("General Info:\r\n    Введите название компании:");
+            string companyName = Console.ReadLine();
+            
+            Console.WriteLine("Owner Info:\r\n    Введите имя владельца компании:");
+            string ownerName = Console.ReadLine();
+            
+            Console.WriteLine("    Введите номер телефона (это будущий логин для входа. Телефон строго в формате +971XXXXXXXXX, т.е. 9 цифр после кода страны):");
+            Regex phoneMask = new Regex("\\+971\\d{9}$");
+            string ownerPhone = Console.ReadLine();
+            while (!phoneMask.IsMatch(ownerPhone))
+            {
+                Console.WriteLine("    Неверный формат номера телефона, попробуйте еще раз:");
+                ownerPhone = Console.ReadLine();
+            }
+            
+            Console.WriteLine("    Введите НАСТОЯЩИЙ E-mail. На него придёт письмо с установкой пароля!!!:");
+            string ownerEmail = Console.ReadLine();
+            */
+
+            var token = Auth.TokenGet("serviceone", "Service1");
+
+            Dictionary<string, object> kek = new Dictionary<string, object>();
+            return kek;
+        }
+    }
+}
